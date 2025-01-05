ee80b829b112ca189f79bb6ef282608a533de5bf
aTrueYety <erikhoff00@gmail.com>
2025-01-05 14:46:25 +0100

    Fix logos and app metadata

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Account from './pages/Account';
 import Lists from './pages/Lists';
+import Transactions from './pages/Transactions';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/account" element={<Account />} />
         <Route path='/lists' element={<Lists />} />
-
+        <Route path='/transactions' element={<Transactions />} />
       </Routes>
     </Router>
   );
